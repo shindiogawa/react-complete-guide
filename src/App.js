13d46c385cb8639ed2ac9afcc078e6d901c9de45
Rodrigo Shindi Ogawa Osiro <rodrigoogawa@Rodrigos-MacBook-Pro.local>
2020-04-13 11:21:06 -0300

    Giving props children

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a react app</h1>
+        <p>This is really working!</p>
+        <Person name="Max" age="28"/>
+        <Person name="Manu" age="29">My hobbies: Racing </Person>
+        <Person name="Stephanie" age="25"/>
       </div>
     );
 
