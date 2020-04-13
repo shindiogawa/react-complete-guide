f1486a6a197325ebeeb151208d158ae147060ca7
Rodrigo Shindi Ogawa Osiro <rodrigoogawa@Rodrigos-MacBook-Pro.local>
2020-04-13 11:33:46 -0300

    Changing a state

--- src/App.js
+++ src/App.js
@@ -9,7 +9,18 @@ class App extends Component {
       {name: "Max", age: 28},
       {name: "Manu", age: 29},
       {name: "Steephanie", age: 25}
-    ]
+    ],
+    otherState: 'some other value'
+  }
+  switchNameHandler = () => {
+    // console.log("Was clicked");
+    // DON'T DO THIS: this.state.persons[0].name = "Maximilian";
+
+    this.setState({persons: [
+      {name: "Maximilian", age: 28},
+      {name: "Manu", age: 29},
+      {name: "Steephanie", age: 27}
+    ]});
   }
 
   render() {
@@ -17,7 +28,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a react app</h1>
         <p>This is really working!</p>
-        <buttom>Switch Name</buttom>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing </Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
